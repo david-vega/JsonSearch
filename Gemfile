5d56a892f74fb08fc745677a0ab0454d168a2f6c
David Vega <me@david-vega.com>
2018-02-09 00:51:33 -0800

    initial commit :star:

---
+++ Gemfile
@@ -0,0 +1,11 @@
+source 'http://rubygems.org'
+
+group :development, :test do
+  gem 'awesome_print'
+  gem 'pry'
+end
+
+group :test do
+  gem 'nyan-cat-formatter'
+  gem 'rspec'
+end
---
+++ config/env.rb
@@ -0,0 +1,12 @@
+require 'rubygems'
+require 'bundler'
+
+ENV['RACK_ENV'] ||= 'development'
+
+Bundler.require(:default, ENV['RACK_ENV'])
+
+root_path = Pathname.new(File.expand_path('..', File.dirname(__FILE__)))
+%w[].each do |directory|
+  Dir[File.expand_path(File.join(root_path, directory,'**','*.rb'))].each {|file| require file }
+end
+
